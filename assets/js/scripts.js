5d7bcc1d9eb748fc4b6672d19707e78f28c3cbde
kamutu <wangechililian@gmail.com>
2022-01-20 17:30:58 +0300

    add constructor function,make changes to pricing ,add a checkbox for the toppings

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+//user interface
+let costSize,costCrust,costTopping,costDelivery;
+let totalCost=0;
+
+
+// pizza cost variables size,crust,toppings
+
+
+//  constructor for ordering pizza
+
+Function Orderpizza(size,crust,toppings,total){
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.total = total;
+}
+
+// Add your cart
